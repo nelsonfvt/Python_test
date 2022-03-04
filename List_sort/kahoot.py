1960ef99f86e02e6f74fe8e93cb9cfcb0224a77c
nelsonfvt <64501607+nelsonfvt@users.noreply.github.com>
2022-03-04 09:41:26 -0500

    Add files via upload

--- List_sort/kahoot.py
+++ List_sort/kahoot.py
@@ -5,9 +5,10 @@ def process_df(kh_file,es_df,quiz):
     #Cargando resultados de Kahoot
     kh_df = pd.read_excel(kh_file, sheet_name='Final Scores',header=2)
     regs=len(kh_df)
-    grds = [0] * regs
+    grds = [0.0] * regs
     kh_df.insert(3, 'Grades', grds)
     kh_df['Player'] = kh_df['Player'].astype(str)
+    kh_df['Player'] = kh_df['Player'].str.strip()
     
     # asignando 50 al podio
     kh_df.loc[kh_df['Rank'] < 4,'Grades'] = 50
@@ -18,13 +19,15 @@ def process_df(kh_file,es_df,quiz):
     
     # calculando percentiles
     perc = np.percentile(np_arr,[25, 50, 75])
+    print('Percentiles (25 - 50 - 75):')
+    print(perc)
     lim_max = kh_df.loc[kh_df['Rank'] == 3,'Total Score (points)'].to_numpy()
 
     # asignando notas segun percentiles
-    kh_df.loc[ (kh_df['Total Score (points)'] < lim_max[0]) & (kh_df['Total Score (points)'] >= perc[2]), 'Grades' ] =45
-    kh_df.loc[ (kh_df['Total Score (points)'] < perc[2]) & (kh_df['Total Score (points)'] >= perc[1]), 'Grades' ] =38
-    kh_df.loc[ (kh_df['Total Score (points)'] < perc[1]) & (kh_df['Total Score (points)'] >= perc[0]), 'Grades' ] =30
-    kh_df.loc[ kh_df['Total Score (points)'] < perc[0], 'Grades' ] =25
+    kh_df.loc[ (kh_df['Total Score (points)'] < lim_max[0]) & (kh_df['Total Score (points)'] >= perc[2]), 'Grades' ] =45.0
+    kh_df.loc[ (kh_df['Total Score (points)'] < perc[2]) & (kh_df['Total Score (points)'] >= perc[1]), 'Grades' ] =38.0
+    kh_df.loc[ (kh_df['Total Score (points)'] < perc[1]) & (kh_df['Total Score (points)'] >= perc[0]), 'Grades' ] =30.0
+    kh_df.loc[ kh_df['Total Score (points)'] < perc[0], 'Grades' ] =25.0
 
     for i in range(0,regs):
         nota = kh_df.iloc[[i]]['Grades'].values[0]
@@ -34,18 +37,20 @@ def process_df(kh_file,es_df,quiz):
 
     return es_df
 
-
+#Rutina Principal
 #archivos con Scores
 file_scores = ['G1.xlsx','G2.xlsx','G3.xlsx']
-#file_scores = ['G3.xlsx']
+
 #archivo listado
 file_ests='PDS2022-1.xlsx'
+
 #Quiz a asinar nota
 quiz = 'Q3'
 
 #Cargando lista de estudiantes
 es_df = pd.read_excel(file_ests, sheet_name='Corte1', header=1)
 es_df.dropna(subset=['Codigo'], inplace=True) # eliminar filas vacias
+#print(es_df.dtypes)
 es_df['Codigo'] = es_df['Codigo'].astype(int)
 es_df['Codigo'] = es_df['Codigo'].astype(str)
 
@@ -54,4 +59,5 @@ for i in file_scores:
     es_df = process_df(i, es_df, quiz)
 
 print('Guandando resultado -> out.xlsx')
+#print(es_df.dtypes)
 es_df.to_excel('out.xlsx')
