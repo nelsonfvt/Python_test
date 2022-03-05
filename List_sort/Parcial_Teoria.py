87d76fb61d551da8872b519d9c10ee07e79bb6f5
nelsonfvt <64501607+nelsonfvt@users.noreply.github.com>
2022-03-05 07:55:42 -0500

    Add files via upload

--- List_sort/Parcial_Teoria.py
+++ List_sort/Parcial_Teoria.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 #Rutina Principal
-#archivos con Scores
+#archivos con Calificaciones
 Parcial = 'Parcial-calificaciones.xlsx'
 
-#archivo listado
+#archivo listado general de estudiantes
 file_ests='PDS2022-1.xlsx'
 
 #Cargando lista de estudiantes por codigo
@@ -25,8 +25,18 @@ ml_df['Codigo'] = ml_df['Codigo'].astype(str)
 #Cargando calificaciones de parcial
 nt_df = pd.read_excel(Parcial)
 
-es_df['Ex_Pr'] = es_df.loc
-es_df.loc[ (ml_df['Correo'] == nt_df['Dirección de correo']) , ''] 
+#Organizar lista de correos de acuerdo con los códigos
+ml_df = ml_df.set_index('Codigo')
+ml_df = ml_df.reindex(index=es_df['Codigo'])
+ml_df = ml_df.reset_index()
 
-for i in range(0,regs):
-    print('a')
+#Organizar lista de notas de acuerdo con los correos organizados
+nt_df = nt_df.set_index('Dirección de correo')
+nt_df = nt_df.reindex(index=ml_df['Dirección de correo'])
+nt_df = nt_df.reset_index()
+
+#Extraer nombres y calificaciones
+out_df = nt_df[["Apellido(s)" ,"Nombre", "Calificación/10.00" ]]
+out_df['Calificación/10.00'] =  out_df['Calificación/10.00'].apply(lambda x: x*5.0)
+print(out_df)
+out_df.to_excel('Notas_parcial.xlsx')
